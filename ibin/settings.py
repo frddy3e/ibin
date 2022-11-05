d6f5a030f767b98754830e6901e81ec73819964e
Freddy Panchi <freddy@touropp.com>
2022-11-05 22:55:01 +0000

    add allowed hosts

--- ibin/settings.py
+++ ibin/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ygn@+(a=*ye9mn(%pd6jt*(=-1-#v=lh&sd2(s7*@!0l&u$yr_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '13.52.184.177']
 
 
 # Application definition
