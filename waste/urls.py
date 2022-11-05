107578da9ebbbe5cfb93f44f4ef5ca37086b47c3
Freddy Panchi <freddy@Freddys-MacBook-Pro.local>
2022-11-05 17:15:35 -0500

    change to relative urls

--- waste/urls.py
+++ waste/urls.py
@@ -2,9 +2,8 @@ from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-        url(r'^estados_waste/$', views.estados_wasteView),
-		url(r'^inicio_waste/$', views.inicio_wasteView),
-        url(r'^historico_waste/$', views.historico_wasteView),
-        url(r'^actualizar_waste/$', views.actualizar_wasteView),
-        
+        url(r'^estados_waste/$', views.estados_wasteView, name='estados_waste'),
+		url(r'^inicio_waste/$', views.inicio_wasteView, name='inicio_waste'),
+        url(r'^historico_waste/$', views.historico_wasteView, name='historico_waste'),
+        url(r'^actualizar_waste/$', views.actualizar_wasteView, name='actualizar_waste'),
         ]
